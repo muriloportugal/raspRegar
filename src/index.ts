086af915aff1c4879c6bafd375eceef7dbcabc34
Murilo Portugal <murilo.portugal@gmail.com>
2023-02-25 18:30:56 -0300

    add http server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-import {hardwareRevision} from 'pigpio';
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import {Gpio} from 'pigpio';
 
-console.log('Hardware Revision: ' + hardwareRevision().toString(16));
+const led = new Gpio(17,{ mode: Gpio.OUTPUT });
+
+const app = Fastify({logger:true});
+app.register(cors);
+
+app.get('/liga',async(request,reply)=>{
+  led.digitalWrite(1);
+  return {message:'ligado'}
+});
+
+app.get('/desligado',async(request,reply)=>{
+  led.digitalWrite(1);
+  return {message:'desligado'}
+});
+
+app.listen({
+  port:3334,
+  host:'0.0.0.0',
+}).then(()=>{
+  console.log('Http Server running on port 3334');
+});
