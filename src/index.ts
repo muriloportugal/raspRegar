ef9570841cabdd66900a1283f5cc708f9e29abc3
Murilo Portugal <murilo.portugal@gmail.com>
2023-02-25 22:06:31 -0300

    correcoes

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,15 @@ import cors from '@fastify/cors';
 import {Gpio} from 'pigpio';
 
 const led = new Gpio(17,{ mode: Gpio.OUTPUT, });
-led.digitalWrite(1); //Já inicia com o pino UP
+led.digitalWrite(1); //Já inicia com o pino UP pois o rele é comum fechado
 const app = Fastify({logger:false});
 app.register(cors);
 
 app.get('/ligar',async(request,reply)=>{
-  led.digitalWrite(0);
-  //Após 3 segundos desliga
+  led.digitalWrite(0);//Liga a bomba dagua
+  
   setTimeout(() => {
+    //Após 3 segundos desliga a bomba dagua
     led.digitalWrite(1);
   }, 3000);
 
