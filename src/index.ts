29a6534fd8a034f19256e6f0315f6d728b7ab9fd
Murilo Portugal <murilo.portugal@gmail.com>
2023-02-25 22:06:24 -0300

    correcoes

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import {Gpio} from 'pigpio';
 
-const led = new Gpio(17,{ mode: Gpio.OUTPUT });
-
+const led = new Gpio(17,{ mode: Gpio.OUTPUT, });
+led.digitalWrite(1); //Já inicia com o pino UP
 const app = Fastify({logger:false});
 app.register(cors);
 
 app.get('/ligar',async(request,reply)=>{
-  led.digitalWrite(1);
+  led.digitalWrite(0);
   //Após 3 segundos desliga
   setTimeout(() => {
-    led.digitalWrite(0);
+    led.digitalWrite(1);
   }, 3000);
 
   return {message:'ligado'}
